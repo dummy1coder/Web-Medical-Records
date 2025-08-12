58318b8b397e312a28e6c69e6f265d259f2d0a81
hnafula99 <hannahnyongesa@gmail.com>
2025-08-12 15:58:19 +0300

    Changes

--- app/Http/Controllers/MedicalRecordsController.php
+++ app/Http/Controllers/MedicalRecordsController.php
@@ -2,92 +2,65 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\MedicalRecord;
-use App\Http\Requests\StoreMedicalRecordRequest;
-use App\Http\Requests\UpdateMedicalRecordRequest;
 use App\Models\MedicalRecords;
-use Illuminate\View\View;
-use Illuminate\Http\RedirectResponse;
+use App\Http\Requests\StoreMedicalRecordsRequest;
+use App\Http\Requests\UpdateMedicalRecordsRequest;
 
 class MedicalRecordsController extends Controller
 {
     /**
-     * Instantiate a new MedicalRecordController instance.
-     */
-    public function __construct()
-    {
-       $this->middleware('auth');
-       $this->middleware('permission:create-medicalrecord|edit-medicalrecord|delete-medicalrecord', ['only' => ['index','show']]);
-       $this->middleware('permission:create-medicalrecord', ['only' => ['create','store']]);
-       $this->middleware('permission:edit-medicalrecord', ['only' => ['edit','update']]);
-       $this->middleware('permission:delete-medicalrecord', ['only' => ['destroy']]);
-    }
-
-    /**
      * Display a listing of the resource.
      */
-    public function index(): View
+    public function index()
     {
-        return view('medicalrecords.index', [
-            'medicalrecords' => MedicalRecords::latest()->paginate(3)
-        ]);
+        //
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create(): View
+    public function create()
     {
-        return view('medicalrecords.create');
+        //
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreMedicalRecordRequest $request): RedirectResponse
+    public function store(StoreMedicalRecordsRequest $request)
     {
-        MedicalRecords::create($request->all());
-        return redirect()->route('medicalrecords.index')
-                ->withSuccess('New medicalrecord is added successfully.');
+        //
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(MedicalRecords $medicalrecord): View
+    public function show(MedicalRecords $medicalRecords)
     {
-        return view('medicalrecords.show', [
-            'medicalrecord' => $medicalrecord
-        ]);
+        //
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(MedicalRecords $medicalrecord): View
+    public function edit(MedicalRecords $medicalRecords)
     {
-        return view('medicalrecords.edit', [
-            'medicalrecord' => $medicalrecord
-        ]);
+        //
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateMedicalRecordRequest $request, MedicalRecords $medicalrecord): RedirectResponse
+    public function update(UpdateMedicalRecordsRequest $request, MedicalRecords $medicalRecords)
     {
-        $medicalrecord->update($request->all());
-        return redirect()->back()
-                ->withSuccess('Medicalrecord is updated successfully.');
+        //
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(MedicalRecords $medicalrecord): RedirectResponse
+    public function destroy(MedicalRecords $medicalRecords)
     {
-        $medicalrecord->delete();
-        return redirect()->route('medicalrecords.index')
-                ->withSuccess('Medicalrecord is deleted successfully.');
+        //
     }
 }
